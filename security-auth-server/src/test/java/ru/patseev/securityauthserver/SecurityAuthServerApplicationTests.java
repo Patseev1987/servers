7c5b3b2d1a0bbab1cb6faa918b802798435cfa16
Bogdan Patseev <patseev1987@gmail.com>
2024-06-08 15:49:16 +0300

    add security server

---
+++ security-auth-server/src/test/java/ru/patseev/securityauthserver/SecurityAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.patseev.securityauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityAuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
